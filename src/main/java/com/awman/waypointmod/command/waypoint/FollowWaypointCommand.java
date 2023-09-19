d3d2bcc11269f0814ca3f2e374aab51d0c35bc90
Aw Man <brambilladaniele@icloud.com>
2023-09-19 18:13:59 +0200

    general development

--- src/main/java/com/awman/waypointmod/command/waypoint/FollowWaypointCommand.java
+++ src/main/java/com/awman/waypointmod/command/waypoint/FollowWaypointCommand.java
@@ -2,12 +2,10 @@ package com.awman.waypointmod.command.waypoint;
 
 import com.awman.waypointmod.command.suggestion.WaypointNameSuggestionProvider;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.ParseResults;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.context.CommandContextBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -25,14 +23,44 @@ public class FollowWaypointCommand {
                         .executes(context -> runUnfollow(context))));
     }
 
-    public static int runFollow(CommandContext<ServerCommandSource> context, String waypointId) {
-        CommandManager commandManager = context.getSource().getServer().getCommandManager();
-        CommandSource playerCommandSource = context.getSource().getPlayer().getCommandSource();
+    public static int runFollow(CommandContext<ServerCommandSource> context, String waypointId) throws CommandSyntaxException {
+        try {
+            ServerPlayerEntity player = context.getSource().getPlayer();
+            String playerName = player.getName().getString();
+            CommandManager commandManager = context.getSource().getServer().getCommandManager();
+            CommandDispatcher<ServerCommandSource> dispatcher = commandManager.getDispatcher();
 
-        commandManager.execute((ParseResults<ServerCommandSource>) playerCommandSource, "title @s title testing");
+            String disableOutput = "gamerule sendCommandFeedback false";
 
-        context.getSource().sendMessage(Text.of("Following waypoint!!!! :)))"));
-        return 1;
+            String commandTimes =
+                    "title " +
+                    playerName +
+                    " times " +
+                    "1 " + // Fade in   \
+                    "20 " + // Stay      } Time in game ticks. TRAILING SPACE IS IMPORTANT
+                    "1"; // Fade out    /
+
+            String command =
+                    "title " +
+                    playerName +
+                    " actionbar {" +
+                    "\"text\" : \"[Waypoint tracking info here]\"," +
+                    "\"color\" : \"red\"" +
+                    "}";
+
+            // Shut up commands!!!
+            commandManager.execute(dispatcher.parse(disableOutput, context.getSource()), disableOutput);
+            // Set the title times
+            commandManager.execute(dispatcher.parse(commandTimes, context.getSource()), commandTimes);
+            // Run the actual title command
+            commandManager.execute(dispatcher.parse(command, context.getSource()), command);
+
+            context.getSource().sendMessage(Text.of("Following waypoint!!!! :)))"));
+            return 1;
+        } catch (Exception e) {
+            context.getSource().sendMessage(Text.of("" + e));
+        }
+        return -1;
     }
 
     public static int runUnfollow(CommandContext<ServerCommandSource> context) {
