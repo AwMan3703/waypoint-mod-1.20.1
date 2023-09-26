a845ef95097c6ce88cc7d07a61211aedd1fffe99
Aw Man <brambilladaniele@icloud.com>
2023-09-26 17:55:45 +0200

    added comments on ModRegistries.java

--- src/main/java/com/awman/waypointmod/util/ModRegistries.java
+++ src/main/java/com/awman/waypointmod/util/ModRegistries.java
@@ -2,13 +2,18 @@ package com.awman.waypointmod.util;
 
 import com.awman.waypointmod.command.waypoint.*;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
 public class ModRegistries {
+    // This class manages the mod's registries (adds things like modded blocks, items and commands to the game)
+    // In this mod's case, we're only adding commands
+
     public static void registerEverything() {
         // Register everything
         registerCommands();
     }
 
     private static void registerCommands() {
+        // For each command, call it's register() method
         CommandRegistrationCallback.EVENT.register(CreateWaypointCommand::register);
         CommandRegistrationCallback.EVENT.register(ListWaypointCommand::register);
         CommandRegistrationCallback.EVENT.register(InfoWaypointCommand::register);
