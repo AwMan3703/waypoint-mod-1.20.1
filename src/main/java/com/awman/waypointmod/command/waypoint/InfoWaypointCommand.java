2a3e37962f043db127dded77bbf1c562e2d0fb80
Aw Man <brambilladaniele@icloud.com>
2023-09-26 22:00:02 +0200

    added comments to InfoWaypointCommand.java

--- src/main/java/com/awman/waypointmod/command/waypoint/InfoWaypointCommand.java
+++ src/main/java/com/awman/waypointmod/command/waypoint/InfoWaypointCommand.java
@@ -13,42 +13,60 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
 public class InfoWaypointCommand {
+    // Class for the /waypoint info command
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
+        // Register under the /waypoint command
         dispatcher.register(CommandManager.literal("waypoint")
+                // Specify the literal "info"
                 .then(CommandManager.literal("info")
+
+                        // Take a mandatory string argument "waypoint_id" to specify the name of the waypoint to inspect
                         .then(CommandManager.argument("waypoint_id", StringArgumentType.string())
+                                // Send custom suggestions, choosing from the name of the available waypoints
                                 .suggests((context, builder) -> new WaypointNameSuggestionProvider().getSuggestions(context, builder))
+                                // Get information about the waypoint
                                 .executes(context -> run(context,
                                     StringArgumentType.getString(context, "waypoint_id"))))));
     }
 
     public static int run(CommandContext<ServerCommandSource> context, String waypointId) throws CommandSyntaxException {
-            try {
-                StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(context.getSource().getWorld().getServer());
+        try {
+            // Get the serverState for managing the server's WaypointMap instance
+            StateSaverAndLoader serverState = StateSaverAndLoader.getServerState(context.getSource().getWorld().getServer());
 
-                if (!serverState.waypointMap.containsKey(waypointId)) {
-
-                    context.getSource().sendMessage(Text.of("Waypoint not found!"));
-
-                    return -1;
-                } else {
-
-                    WaypointData waypointData = serverState.waypointMap.get(waypointId);
-
-                    context.getSource().sendMessage(Text.of("Waypoint data:"));
-                    context.getSource().sendMessage(Text.of(
-                            "-> \"" + waypointId + "\", created by " +
-                                    "@" + waypointData.author + " (" +
-                                    (waypointData.isPublic() ? "public" : "private") + "): [" +
-                                    waypointData.coordinates.toShortString() + " in " +
-                                    waypointData.dimension.toString() + "]"));
-
-                    return 1;
-                }
-            } catch (Exception e) {
-                context.getSource().sendMessage(Text.of("WPM ERROR: " + e));
+            // If the server's WaypointMap instance does not contain the chosen waypoint:
+            if (!serverState.waypointMap.containsKey(waypointId)) {
+                // Inform the player via a chat message
+                context.getSource().sendMessage(Text.of("Waypoint not found!"));
+                // Return -1 (command execution failed)
                 return -1;
+            } else {
+                // If the server's WaypointMap instance DOES contain the chosen waypoint:
+
+                // Get the waypoint's data
+                WaypointData waypointData = serverState.waypointMap.get(waypointId);
+
+                // Send the header
+                context.getSource().sendMessage(Text.of("Waypoint data:"));
+
+                // Send the waypoint's info (name, author, visibility (not implemented), coordinates and dimension)
+                context.getSource().sendMessage(Text.of(
+                        "-> \"" + waypointId + "\", created by " +
+                                "@" + waypointData.author + " (" +
+                                (waypointData.isPublic() ? "public" : "private") + "): [" +
+                                waypointData.coordinates.toShortString() + " in " +
+                                waypointData.dimension.toString() + "]"));
+
+                // Return 1 (command executed successfully)
+                return 1;
             }
+        } catch (Exception e) {
+            // Print any exception to the chat
+            context.getSource().sendMessage(Text.of("WPM ERROR: " + e));
+            // Return -1 (command execution failed)
+            return -1;
+        }
     }
 }
 
