91f795bad4a369c580705d33b8442af9e67241e3
Aw Man <brambilladaniele@icloud.com>
2023-09-26 17:52:29 +0200

    added comments on WaypointMod.java

--- src/main/java/com/awman/waypointmod/WaypointMod.java
+++ src/main/java/com/awman/waypointmod/WaypointMod.java
@@ -6,8 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WaypointMod implements ModInitializer {
-	// TODO: add comments on everything
+	// This class is the main entry point for the mod's initialization
 
+	// Mod's id constant, needed in many files
 	public static final String MOD_ID = "waypoint-mod";
 
 	// This logger is used to write text to the console and the log file.
@@ -15,14 +16,13 @@ public class WaypointMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// What permission level is considered op by the mod
 	public static final int opPermissionLevel = 1;
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		// However, some things (like resources) may still be uninitialized, but this mod doesn't really need them.
 		LOGGER.info("Registering stuff");
 		ModRegistries.registerEverything();
 
